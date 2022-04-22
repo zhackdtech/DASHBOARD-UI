4706216ee8a0f74f8dd096a3faf464cafc3f0517
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2022-04-22 14:22:38 +0800

    ui

--- DASHBOARD-UI/Components/Ellipse.cs
+++ DASHBOARD-UI/Components/Ellipse.cs
@@ -8,7 +8,6 @@ namespace DASHBOARD_UI.Components
 {
     internal class Ellipse : Component
     {
-        private Control hostControl;
         [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
         private static extern IntPtr CreateRoundRectRgn
             (
